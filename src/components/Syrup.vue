226d90465f6f1beb0d42ef5fa4652ca52db52822
prosperouspi <115018951+prosperouspi@users.noreply.github.com>
2024-11-01 20:36:54 -0400

    Deleted src/src and added syrup option

--- src/components/Syrup.vue
+++ src/components/Syrup.vue
@@ -1,8 +1,23 @@
 <template>
-  <div class="syrup"></div>
+  <div class="syrup">
+    <div v-for=" in 5" class="syrup" :style="applyStyle"></div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+
+import {currentSyrup} from '../stores/beverage';
+import {computed} from 'vue';
+const applyStyle = computed(() => {
+    return {
+        backgroundColor: currentSyrup.value.color
+    };
+})
+
+
+
+</script>
 <style lang="scss" scoped>
 .syrup {
   transform: translateY(400%);
